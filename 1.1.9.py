3fcd4da7048ca795ec45747c09bbee232552ac29
Rowan Nicholas <78248776+rowannicholas1@users.noreply.github.com>
2023-02-08 12:22:24 -0800

    basic sprinkles more work needed

--- 1.1.9.py
+++ 1.1.9.py
@@ -12,16 +12,44 @@ p.pendown()
 p.begin_fill()
 p.circle(350, 360, 4)
 p.end_fill()
+p.ht()
 
 #draw bread
-p.pencolor("#964B00")
-p.fillcolor("#964B00")
-p.penup()
-p.goto(0, -20)
-p.pendown()
-p.begin_fill()
-p.circle(200, 360)
-p.end_fill()
+b = trtl.Turtle()
+b.speed(0)
+b.pencolor("#964B00")
+b.fillcolor("#964B00")
+b.penup()
+b.goto(0, -20)
+b.pendown()
+b.begin_fill()
+b.circle(200)
+b.penup()
+b.goto(0, 125)
+b.pendown()
+b.circle(50)
+b.end_fill()
+b.ht()
+
+s = trtl.Turtle()
+s.speed(0)
+sprinkle_color = ["red", "yellow", "blue", "green", "purple", "white"]
+sprinkle_shape = []
+sprinklex = 20
+sprinkley = 20
+direction = 90
+for color in range(6):
+    new_color = sprinkle_color.pop()
+    s.pencolor(new_color)
+    s.fillcolor(new_color)
+    s.penup()
+    s.goto(sprinklex, sprinkley)
+    s.pendown()
+    s.circle(5)
+    sprinklex = s.xcor() + 20
+    sprinkley = s.ycor() + 20
+    direction += 10
+s.ht()
 
 wn = trtl.Screen()
 wn.mainloop()
