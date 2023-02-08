74f9b7c14276f037aa6d0c6e73a33b3ff0320279
Rowan Nicholas <78248776+rowannicholas1@users.noreply.github.com>
2023-02-08 11:39:23 -0800

    donut added

--- 1.1.9.py
+++ 1.1.9.py
@@ -3,13 +3,24 @@ import turtle as trtl
 p = trtl.Turtle()
 p.speed(0)
 
-p.penup()
-p.goto(0, -150)
-p.pendown()
+#draw plate
 p.pencolor("#c1cdcd")
 p.fillcolor("#c1cdcd")
+p.penup()
+p.goto(0, -175)
+p.pendown()
 p.begin_fill()
-p.circle(300, 360, 4)
+p.circle(350, 360, 4)
+p.end_fill()
+
+#draw bread
+p.pencolor("#964B00")
+p.fillcolor("#964B00")
+p.penup()
+p.goto(0, -20)
+p.pendown()
+p.begin_fill()
+p.circle(200, 360)
 p.end_fill()
 
 wn = trtl.Screen()
