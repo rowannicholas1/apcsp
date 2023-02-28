95bba1388c55c49c1b9a5b6ec7e74f768eb4592d
Rowan Nicholas <78248776+rowannicholas1@users.noreply.github.com>
2023-02-28 08:50:54 -0800

    txt

--- 1.2/1.2.1.py
+++ 1.2/1.2.1.py
@@ -1,5 +1,9 @@
 import turtle as trtl
 import random as rand
+import leaderboard as lb
+
+leaderboard_file_name = "a122_leaderboard.txt"
+player_name = input("Please enter your name:")
 
 spot = trtl.Turtle()
 score_writer = trtl.Turtle()
@@ -14,7 +18,7 @@ spot_size = 2
 score = 0
 font_setup = ("Times New Roman", 20)
 
-timer = 30
+timer = 5  # 30
 counter_interval = 1000
 timer_up = False
 
@@ -28,6 +32,21 @@ score_writer.goto(-200, 200)
 
 counter.penup()
 counter.goto(-175, 200)
+
+
+def manage_leaderboard():
+    global score
+    global spot
+    leader_names_list = lb.get_names(leaderboard_file_name)
+    leader_scores_list = lb.get_scores(leaderboard_file_name)
+    if (len(leader_scores_list) < 5 or score >= leader_scores_list[4]):
+        lb.update_leaderboard(leaderboard_file_name, leader_names_list,
+                              leader_scores_list, player_name, score)
+        lb.draw_leaderboard(True, leader_names_list,
+                            leader_scores_list, spot, score)
+    else:
+        lb.draw_leaderboard(False, leader_names_list,
+                            leader_scores_list, spot, score)
 
 
 def change_position():
@@ -45,6 +64,7 @@ def countdown():
         spot.ht()
         score_writer.clear()
         score_writer.write(score, font=font_setup)
+        manage_leaderboard()
     else:
         counter.write("Timer: " + str(timer), font=font_setup)
         timer -= 1
