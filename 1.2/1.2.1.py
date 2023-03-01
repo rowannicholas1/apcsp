d983b774f5750573e539bff2ab1e3afe22e6161a
Rowan Nicholas <78248776+rowannicholas1@users.noreply.github.com>
2023-03-01 11:15:44 -0800

    78

--- 1.2/1.2.1.py
+++ 1.2/1.2.1.py
@@ -1,19 +1,17 @@
-# a121_catch_a_turtle.py
-# -----import statements-----
 import turtle as trtl
 import random as rand
 import leaderboard as lb
-# -----game configuration----
+
 color1 = ("green")
 size1 = 2
 shape1 = ("circle")
 score = 0
-timer = 1
+timer = 5
 counter_interval = 1000  # 1000 represents 1 second
 timer_up = False
 leaderboard_file_name = "leaderboard.txt"
 player_name = input("Please enter your name:")
-# -----initialize turtle-----
+
 wn = trtl.Screen()
 counter = trtl.Turtle()
 counter.ht()
@@ -27,9 +25,7 @@ spot.penup()
 scw = trtl.Turtle()
 scw.penup()
 scw.ht()
-# score goes to top left
 scw.goto(-160, 175)
-# -----game functions--------
 
 
 def countdown():
@@ -57,7 +53,7 @@ def update_score():
 def change_position():
     x1 = rand.randint(-150, 150)
     y1 = rand.randint(-150, 150)
-    sp.goto(x1, y1)
+    spot.goto(x1, y1)
 
 
 def spot_clicked(x, y):
@@ -71,27 +67,20 @@ def spot_clicked(x, y):
 
 
 def manage_leaderboard():
-
     global score
     global spot
-
-    # get the names and scores from the leaderboard file
     leader_names_list = lb.get_names(leaderboard_file_name)
     leader_scores_list = lb.get_scores(leaderboard_file_name)
-
-    # show the leaderboard with or without the current player
     if (len(leader_scores_list) < 5 or score >= leader_scores_list[4]):
         lb.update_leaderboard(leaderboard_file_name, leader_names_list,
                               leader_scores_list, player_name, score)
         lb.draw_leaderboard(True, leader_names_list,
                             leader_scores_list, spot, score)
-
     else:
         lb.draw_leaderboard(False, leader_names_list,
                             leader_scores_list, spot, score)
 
 
 spot.onclick(spot_clicked)
-# -----events----------------
 wn.ontimer(countdown, counter_interval)
 wn.mainloop()
