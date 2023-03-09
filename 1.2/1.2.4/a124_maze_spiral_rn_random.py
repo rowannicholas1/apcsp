3c807a40d9118917807781d9a74f83d47fbf3b0d
Rowan Nicholas <78248776+rowannicholas1@users.noreply.github.com>
2023-03-09 09:30:47 -0800

    doen with maze

--- 1.2/1.2.4/a124_maze_spiral_rn_random.py
+++ 1.2/1.2.4/a124_maze_spiral_rn_random.py
@@ -17,7 +17,7 @@ PATH_WIDTH = 20
 
 def maze_forward():
     maze_painter.left(90)
-    maze_painter.forward(DISTANCE*1.7)
+    maze_painter.forward(DISTANCE*1.6)
 
 
 def maze_forward_rand():
@@ -42,13 +42,23 @@ def maze_barrier():
     maze_painter.forward((DISTANCE - rand_distance)/2)
 
 
+def maze_barrier_outside():
+    maze_painter.forward(20)
+    maze_painter.left(90)
+    maze_painter.forward(PATH_WIDTH*2)
+    maze_painter.back(PATH_WIDTH*2)
+    maze_painter.right(90)
+    maze_painter.forward((DISTANCE - rand_distance)/2)
+
+
 for wall in range(40):
     rand_distance = rand.randint(0, DISTANCE)
     if wall <= 4:
         maze_forward()
         maze_forward()
-    elif wall >= 38:
+    elif wall >= 36:  # doesn't work as intended
         maze_forward()
+        maze_barrier_outside()
     else:
         door_or_barrier = rand.randint(0, 1)
         if door_or_barrier == 0:
