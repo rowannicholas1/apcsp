f14ff008ad0d952e2b980b45fb6d00cbe96af110
Rowan Nicholas <78248776+rowannicholas1@users.noreply.github.com>
2023-03-09 09:16:33 -0800

    up

--- 1.2/1.2.4/a124_maze_spiral_rn_random.py
+++ 1.2/1.2.4/a124_maze_spiral_rn_random.py
@@ -17,34 +17,48 @@ PATH_WIDTH = 20
 
 def maze_forward():
     maze_painter.left(90)
-    maze_painter.forward(DISTANCE)
+    maze_painter.forward(DISTANCE*1.7)
+
+
+def maze_forward_rand():
+    maze_painter.left(90)
+    maze_painter.forward(rand_distance)
 
 
 def maze_door():
-    maze_painter.forward(rand.randint(20, 80))
+    maze_painter.forward(20)
     maze_painter.penup()
     maze_painter.forward(PATH_WIDTH*2)
     maze_painter.pendown()
+    maze_painter.forward((DISTANCE - rand_distance)/2)
 
 
 def maze_barrier():
-    maze_painter.forward(rand.randint(20, 80))
+    maze_painter.forward(20)
     maze_painter.right(90)
     maze_painter.forward(PATH_WIDTH*2)
     maze_painter.back(PATH_WIDTH*2)
     maze_painter.left(90)
-    # maze_painter.forward(20)
+    maze_painter.forward((DISTANCE - rand_distance)/2)
 
 
-for wall in range(30):
-    if wall <= 2:
+for wall in range(40):
+    rand_distance = rand.randint(0, DISTANCE)
+    if wall <= 4:
         maze_forward()
-    elif wall >= 26:
+        maze_forward()
+    elif wall >= 38:
         maze_forward()
     else:
-        maze_forward()
-        maze_door()
-        maze_barrier()
+        door_or_barrier = rand.randint(0, 1)
+        if door_or_barrier == 0:
+            maze_forward_rand()
+            maze_door()
+            maze_barrier()
+        elif door_or_barrier == 1:
+            maze_forward_rand()
+            maze_door()
+            maze_barrier()
     DISTANCE += 20
 
 wn.listen()
